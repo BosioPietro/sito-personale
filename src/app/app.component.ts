5acf13b939c74506f0fa8149172aed04388fc99b
BosioPietro <113420960+BosioPietro@users.noreply.github.com>
2024-08-02 23:35:10 +0200

    Integrato header con scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { SezionePrincipaleComponent } from "./sezione-principale/sezione-principale.component";
@@ -13,32 +13,99 @@ import { FooterContattiComponent } from "./footer-contatti/footer-contatti.compo
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
+  
   title = 'Progetto';
 
   sezioni?: HTMLElement[];
   timeoutScroll: any[] = [];
+  disattivaSnap: any = undefined;
+  sezioneCorrente?: string;
+  DISTANZA_SCROLL_SNAP: number = 200;
+
+  constructor(private cd: ChangeDetectorRef) {}
+
+  @ViewChild("wrapperSezioni")
+  wrapperSezioni!: ElementRef<HTMLElement>;
+
+  ngAfterViewInit(): void {
+    this.SezionePagina()
+    this.cd.detectChanges()
+  }
 
   Scrolla(e: Event){
-    this.sezioni = this.sezioni ?? Array.from(document.querySelectorAll('.snap'))!
+    // controllo che la navigazione sia scrollando
+    // e non con navigazione da header
+    if(!this.disattivaSnap){
+      this.SezionePagina();
+    }
+
+    this.SnappaScroll(e);
+  }
+
+  SezionePagina(){
+    if(!this.sezioni){
+      const figli = this.wrapperSezioni.nativeElement.children;
+      const sezioni = Array.from(figli).map((s) => s as HTMLElement);
+      this.sezioni = sezioni;
+    }
+
+    const distanze = this.sezioni!.map((s) => {
+      const ris = { el: s, distanza: NaN }
+      
+      if(s.id == "contatti"){
+        const b = s.getBoundingClientRect().bottom;
+        ris["distanza"] = Math.abs(window.innerHeight - b);
+      }
+      else ris["distanza"] = Math.abs(this.Distanza(s));
+      
+      return ris;
+    }).sort((a, b) => a["distanza"] - b["distanza"]);
+
+    this.sezioneCorrente = distanze[0].el.id;
+
+    if(this.sezioneCorrente == "principale"){
+      this.sezioneCorrente = "conoscenze"
+    }
+  }
+
+  NavigazioneBottoni(sezione: string){
+    this.sezioneCorrente = sezione;
+    clearTimeout(this.disattivaSnap)
+    this.disattivaSnap = setTimeout(() => this.disattivaSnap = undefined, 50)
+  }
+
+  SnappaScroll(e: Event){
+    // usato perchè la navigazione coi bottoni lo triggera.
+    // Con scrollIntoView non ce modo per vedere quando ha
+    // finito, quindi checko periodicamente
+    if(this.disattivaSnap){
+      clearTimeout(this.disattivaSnap);
+      this.disattivaSnap = setTimeout(() => this.disattivaSnap = undefined, 50);
+      this.timeoutScroll = this.timeoutScroll.flatMap((t) => {
+        clearInterval(t);
+        return [];
+      })
+      return;
+    }
+
+    const sezioniSnap = this.sezioni!.filter((s) => s.classList.contains("snap"));
     clearTimeout(this.timeoutScroll.shift());
 
-    const distanze = this.sezioni!.map((s) => ({
+    const distanze = sezioniSnap!.map((s) => ({
       el: s,
       distanza: Math.abs(this.Distanza(s))
     })).sort((a, b) => a["distanza"] - b["distanza"])
 
     const { el, distanza } = distanze[0];
 
-    if(distanza > 300) return;
-
+    if(distanza > this.DISTANZA_SCROLL_SNAP) return;
     
     const main = e.currentTarget as HTMLElement;
     this.timeoutScroll.push(
       setTimeout(() => {
-
         this.SmoothScrollTo(main, el);
-      }, 1000)
+      }, 750)
     );
   }
 
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'Header',
   standalone: true,
   imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  @Input("sezione-corrente")
+  sezione?: string;
+
+  @ViewChild("wrapper")
+  wrapper!: ElementRef<HTMLElement>;
+
+  @ViewChild("cella")
+  cella!: ElementRef<HTMLElement>;
+
+  @Output()
+  onNaviga = new EventEmitter<string>();
+
+  ultimo: {
+    sezione?: string,
+    valore?: string
+  } = {}
+  
+  timeoutTransizione: any;
+  animando: boolean = false;
+
+
   Scrolla(s: string){
     const el = document.getElementById(s)!;
-    el.scrollIntoView({behavior: "smooth"})
+    el.scrollIntoView({behavior: "smooth"});
+    this.onNaviga.emit(s)
+  }
+
+  SezioneCorrente(sezione: string | undefined){
+    if(!sezione) return 0;
+    if(this.ultimo.sezione == sezione) return this.ultimo.valore; 
+
+    const cella = this.wrapper.nativeElement.querySelector(`[sezione="${sezione}"] .cella`)! as HTMLElement;
+
+    const header = this.wrapper.nativeElement.parentElement! as HTMLElement;
+    const offset = header.getBoundingClientRect().left;
+    let valore = `${cella.getBoundingClientRect().left - offset}px`;
+
+    this.ultimo = { sezione, valore };
+    
+    this.animando = true;
+    clearTimeout(this.timeoutTransizione);
+    this.timeoutTransizione = setTimeout(() => {
+      this.animando = false;
+    }, 300);
+
+    return valore;
   }
 }
