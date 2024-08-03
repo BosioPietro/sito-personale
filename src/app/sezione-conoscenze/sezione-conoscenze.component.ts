bfdd801b29c64bb54e2217f3ab6cbc18b66ea811
BosioPietro <113420960+BosioPietro@users.noreply.github.com>
2024-08-03 23:29:05 +0200

    Iniziato contenuto sezione competenze

--- src/app/sezione-conoscenze/sezione-conoscenze.component.ts
+++ src/app/sezione-conoscenze/sezione-conoscenze.component.ts
@@ -5,10 +5,24 @@ import { SfondoComponent } from './sfondo/sfondo.component';
 @Component({
   selector: 'SezioneConoscenze',
   standalone: true,
+  schemas: [],
   imports: [SwitchComponent, SfondoComponent],
   templateUrl: './sezione-conoscenze.component.html',
   styleUrl: './sezione-conoscenze.component.scss'
 })
 export class SezioneConoscenzeComponent {
+
+  EffettoMouse(e: MouseEvent){
+    const cont = e.currentTarget as HTMLElement;
+    const celle = Array.from(cont.children) as HTMLElement[];
+    const { clientX, clientY } = e;
+
+    for(const cella of celle) {
+      const { left, top } = cella.getBoundingClientRect();
+  
+      cella.style.setProperty("--mouse-x", `${clientX - left}px`);
+      cella.style.setProperty("--mouse-y", `${clientY - top}px`);
+    };
+  }
 
 }
