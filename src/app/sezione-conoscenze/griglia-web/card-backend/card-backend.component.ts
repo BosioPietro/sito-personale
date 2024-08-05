6224390f3eb758a34ceabf1a1b15ec1f5669b419
BosioPietro <113420960+BosioPietro@users.noreply.github.com>
2024-08-05 21:30:16 +0200

    Back-end card

---
+++ src/app/sezione-conoscenze/griglia-web/card-backend/card-backend.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'CardBackend',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-backend.component.html',
+  styleUrl: './card-backend.component.scss'
+})
+export class CardBackendComponent {
+
+}
--- src/app/sezione-conoscenze/griglia-web/griglia-web.component.ts
+++ src/app/sezione-conoscenze/griglia-web/griglia-web.component.ts
@@ -1,28 +1,15 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { EffettoMouse } from '../sezione-conoscenze.component';
+import { CardFrontendComponent } from "./card-frontend/card-frontend.component";
+import { CardBackendComponent } from "./card-backend/card-backend.component";
 
 @Component({
   selector: 'GrigliaWeb',
   standalone: true,
-  imports: [],
+  imports: [CardFrontendComponent, CardBackendComponent],
   templateUrl: './griglia-web.component.html',
   styleUrl: './griglia-web.component.scss'
 })
-export class GrigliaWebComponent implements AfterViewInit{
+export class GrigliaWebComponent{
   EffettoMouse = EffettoMouse;
-
-  ngAfterViewInit(): void {
-
-  }
-
-  PosizioneLogo(nLoghi: number, i: number) {
-    const angolo = (2 * Math.PI) / nLoghi;
-    const angoloIndice = angolo * i;
-    
-    const left = `${Math.cos(angoloIndice) * 50}%`
-    const top = `${Math.sin(angoloIndice) * 50}%`
-
-    return {left : `calc(50% - ${left})`, top: `calc(50% - ${top})`}
-  }
-
 }
