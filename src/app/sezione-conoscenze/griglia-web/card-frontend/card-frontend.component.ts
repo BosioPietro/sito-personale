a4511aea1fec3fad0174f551da11b4afe0f1fb79
BosioPietro <113420960+BosioPietro@users.noreply.github.com>
2024-08-05 17:15:41 +0200

    Riorganizzata struttura griglia web

---
+++ src/app/sezione-conoscenze/griglia-web/card-frontend/card-frontend.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'CardFrontend',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-frontend.component.html',
+  styleUrl: './card-frontend.component.scss'
+})
+export class CardFrontendComponent {
+  
+  PosizioneLogo(nLoghi: number, i: number) {
+    const angolo = (2 * Math.PI) / nLoghi;
+    const angoloIndice = angolo * i;
+    
+    const left = `${Math.cos(angoloIndice) * 50}%`
+    const top = `${Math.sin(angoloIndice) * 50}%`
+
+    return {left : `calc(50% - ${left})`, top: `calc(50% - ${top})`}
+  }
+}
