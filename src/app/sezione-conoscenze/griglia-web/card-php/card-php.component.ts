c236f04867747285d041ca86fe7f6bb7afe8457b
BosioPietro <113420960+BosioPietro@users.noreply.github.com>
2024-08-06 00:30:28 +0200

    Creata card PHP

---
+++ src/app/sezione-conoscenze/griglia-web/card-php/card-php.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'CardPhp',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-php.component.html',
+  styleUrl: './card-php.component.scss'
+})
+export class CardPhpComponent {
+
+}
--- src/app/sezione-conoscenze/griglia-web/griglia-web.component.ts
+++ src/app/sezione-conoscenze/griglia-web/griglia-web.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { EffettoMouse } from '../sezione-conoscenze.component';
 import { CardFrontendComponent } from "./card-frontend/card-frontend.component";
 import { CardBackendComponent } from "./card-backend/card-backend.component";
+import { CardPhpComponent } from "./card-php/card-php.component";
 
 @Component({
   selector: 'GrigliaWeb',
   standalone: true,
-  imports: [CardFrontendComponent, CardBackendComponent],
+  imports: [CardFrontendComponent, CardBackendComponent, CardPhpComponent],
   templateUrl: './griglia-web.component.html',
   styleUrl: './griglia-web.component.scss'
 })
