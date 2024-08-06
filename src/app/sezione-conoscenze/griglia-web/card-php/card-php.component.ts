be1b503708d4cbf6585a2b9588a303ecd893a19f
BosioPietro <113420960+BosioPietro@users.noreply.github.com>
2024-08-06 19:31:50 +0200

    Terminata card PHP

--- src/app/sezione-conoscenze/griglia-web/card-php/card-php.component.ts
+++ src/app/sezione-conoscenze/griglia-web/card-php/card-php.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'CardPhp',
   standalone: true,
   imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './card-php.component.html',
   styleUrl: './card-php.component.scss'
 })
