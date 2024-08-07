4c22bdeeb9c15a61da52213dce19c1336c2c057c
BosioPietro <113420960+BosioPietro@users.noreply.github.com>
2024-08-07 21:54:32 +0200

    Aggiunta card ASP

---
+++ src/app/sezione-conoscenze/griglia-web/card-asp/card-asp.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'CardASP',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-asp.component.html',
+  styleUrl: './card-asp.component.scss'
+})
+export class CardASPComponent {
+
+}
