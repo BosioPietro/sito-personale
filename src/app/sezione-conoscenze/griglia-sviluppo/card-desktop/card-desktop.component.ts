676a106a499788f3dd7ab52b85926ed7c39da3a9
BosioPietro <113420960+BosioPietro@users.noreply.github.com>
2024-08-12 00:03:49 +0200

    Card desktop

--- src/app/sezione-conoscenze/griglia-sviluppo/card-desktop/card-desktop.component.ts
+++ src/app/sezione-conoscenze/griglia-sviluppo/card-desktop/card-desktop.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'CardDesktop',
   standalone: true,
   imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './card-desktop.component.html',
   styleUrl: './card-desktop.component.scss'
 })
