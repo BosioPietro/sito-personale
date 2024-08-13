f712e064ac85a78e3a93c0da6c4024549dbf75e5
BosioPietro <113420960+BosioPietro@users.noreply.github.com>
2024-08-13 16:45:43 +0200

    Aggiustate animazioni sviluppo

--- src/app/sezione-conoscenze/griglia-sviluppo/card-database/card-database.component.ts
+++ src/app/sezione-conoscenze/griglia-sviluppo/card-database/card-database.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'CardDatabase',
@@ -7,6 +7,25 @@ import { Component } from '@angular/core';
   templateUrl: './card-database.component.html',
   styleUrl: './card-database.component.scss'
 })
-export class CardDatabaseComponent {
+export class CardDatabaseComponent implements AfterViewInit{
   div = new Array(5);
+
+  @ViewChild("tbody")
+  tbody!: ElementRef<HTMLElement>;
+
+  ngAfterViewInit(): void {
+    const NOME_CLASSE = "hl";
+    const cont = this.tbody.nativeElement;
+    const celle = Array.from(cont.querySelectorAll("td"));
+
+    setInterval(() => {
+      const numero = Math.floor(Math.random() * celle.length);
+      const td = celle[numero];
+
+      if(td.classList.contains(NOME_CLASSE)) return;
+
+      td.classList.add(NOME_CLASSE);
+      setTimeout(() => td.classList.remove(NOME_CLASSE), 500);
+    }, 100);
+  }
 }
