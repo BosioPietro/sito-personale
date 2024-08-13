91b23e0be7c8a6c720b17a46ef413d42d28694f3
BosioPietro <113420960+BosioPietro@users.noreply.github.com>
2024-08-13 16:24:49 +0200

    Card SQL terminata

--- src/app/sezione-conoscenze/griglia-sviluppo/card-sql/card-sql.component.ts
+++ src/app/sezione-conoscenze/griglia-sviluppo/card-sql/card-sql.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'CardSQL',
   standalone: true,
   imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './card-sql.component.html',
   styleUrl: './card-sql.component.scss'
 })
