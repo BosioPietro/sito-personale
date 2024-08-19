b35df4d68cc7b5a242888e14fb8621a4a06a208d
BosioPietro <113420960+BosioPietro@users.noreply.github.com>
2024-08-19 14:12:02 +0200

    Completato footer

--- README.md
+++ README.md
@@ -5,7 +5,6 @@
 - [] Lightmode con invert
 - [x] Vedi se filtro su immagine non selezionata è decente
 - [x] Vite nelle tecnologie del registro
-- [] Fare l'ombra interna dello scroll con bg local
 - [x] Fare il colore di selezione più chiaro
 - [x] Bottone per ingrandire l'immagine
 - [x] Fixare carta asp e guardare se ci sono altri bug
--- src/app/footer-contatti/footer-contatti.component.ts
+++ src/app/footer-contatti/footer-contatti.component.ts
@@ -1,4 +1,4 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { AfterViewInit, Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, ViewChild } from '@angular/core';
 import { SfondoComponent } from './sfondo/sfondo.component';
 
 @Component({
@@ -9,6 +9,35 @@ import { SfondoComponent } from './sfondo/sfondo.component';
   templateUrl: './footer-contatti.component.html',
   styleUrl: './footer-contatti.component.scss'
 })
-export class FooterContattiComponent {
+export class FooterContattiComponent implements AfterViewInit{
+  @ViewChild("sfondo")
+  sfondo!: ElementRef<HTMLElement>;
 
+  elementi?: HTMLElement[];
+
+  ngAfterViewInit(): void {
+    const o1 = new IntersectionObserver((e) => this.Interseca(e), { threshold: .5 });
+    const o2 = new IntersectionObserver((e) => this.Interseca(e), { threshold: 0 });
+
+    o1.observe(this.sfondo.nativeElement);
+    o2.observe(this.sfondo.nativeElement);
+  }
+
+  Interseca(e: IntersectionObserverEntry[]){
+    const intersectionRatio = Math.floor(e[0].intersectionRatio * 10) / 10;
+    const sfondo = this.sfondo.nativeElement;
+
+    if(!this.elementi){
+      this.elementi =  Array.from(sfondo.parentElement!.getElementsByClassName("animato")) as HTMLElement[];
+    }
+
+    if(!intersectionRatio){
+      this.elementi.forEach((el) => el.classList.remove("appari"))
+    }
+    else if(intersectionRatio >= .33){
+      setTimeout(() => {
+        this.elementi!.forEach((el) => el.classList.add("appari"))
+      });
+    }
+  }
 }
