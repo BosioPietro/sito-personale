c2ddd86cb97c1185aad51d6f13915e3ae9b8083c
Bosio Pietro <113420960+BosioPietro@users.noreply.github.com>
2025-06-07 19:04:39 +0200

    Split changes on tailwind branch

--- src/app/sezione-conoscenze/switch/switch.component.ts
+++ src/app/sezione-conoscenze/switch/switch.component.ts
@@ -1,4 +1,12 @@
-import { AfterViewInit, Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { SwitchService } from './switch.service';
 
 @Component({
@@ -6,66 +14,66 @@ import { SwitchService } from './switch.service';
   standalone: true,
   imports: [],
   templateUrl: './switch.component.html',
-  styleUrl: './switch.component.scss', 
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  styleUrl: './switch.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class SwitchComponent implements AfterViewInit{
-
-  @ViewChild("web")
+export class SwitchComponent implements AfterViewInit {
+  @ViewChild('web')
   opzione_web!: ElementRef<HTMLElement>;
 
-  @ViewChild("sviluppo")
+  @ViewChild('sviluppo')
   opzione_sviluppo!: ElementRef<HTMLElement>;
 
-  @ViewChild("certificazioni")
+  @ViewChild('certificazioni')
   opzione_certificazioni!: ElementRef<HTMLElement>;
 
-  @ViewChild("cella")
+  @ViewChild('cella')
   cella!: ElementRef<HTMLElement>;
 
   @Output()
   onChange = new EventEmitter<Sezioni>();
 
-  constructor(private valore_switch: SwitchService){}
+  constructor(private valore_switch: SwitchService) {}
 
   sezioni = Sezioni;
   sezioneCorrente?: Sezioni;
 
-  async Seleziona(e: Event | HTMLElement, sezione: Sezioni){
-    const el = e instanceof Event ? e.currentTarget! as HTMLElement : e;
+  async Seleziona(e: Event | HTMLElement, sezione: Sezioni) {
+    const el = e instanceof Event ? (e.currentTarget! as HTMLElement) : e;
     const cella = this.cella.nativeElement;
 
     // Tiene conto della width iniziale della cella
     this.ImpostaWidthCella(cella);
-    Object.assign(cella, {bottone: el});
-    
+    Object.assign(cella, { bottone: el });
+
     const w = getComputedStyle(el).width;
     const sinistra = el.offsetLeft;
 
     cella.style.width = w;
     cella.style.left = `${sinistra}px`;
-    
+
     el.parentNode!.childNodes.forEach((b) => {
-      (b as HTMLElement).classList.remove("selezionato", "anima")
-    })
+      (b as HTMLElement).classList.remove('selezionato', 'anima');
+    });
 
-    el.classList.add("selezionato", "anima");
+    el.classList.add('selezionato', 'anima');
 
-    if(this.sezioneCorrente === sezione)return;
+    if (this.sezioneCorrente === sezione) return;
 
     this.sezioneCorrente = sezione;
-    this.onChange.emit(sezione)
+    this.onChange.emit(sezione);
   }
 
-  ImpostaWidthCella(cella: any){
-    const bottone = cella.bottone as HTMLElement || this.opzione_web.nativeElement;
+  ImpostaWidthCella(cella: any) {
+    const bottone =
+      (cella.bottone as HTMLElement) || this.opzione_web.nativeElement;
     const w = getComputedStyle(bottone).width;
     const sinistra = bottone.offsetLeft;
-    
-    cella.classList.add("no-transizione")
+
+    cella.classList.add('no-transizione');
     cella.style.width = w;
     cella.style.left = `${sinistra}px`;
-    cella.classList.remove("no-transizione")
+    cella.classList.remove('no-transizione');
   }
 
   ngAfterViewInit(): void {
@@ -73,12 +81,12 @@ export class SwitchComponent implements AfterViewInit{
       [Sezioni.Web]: this.opzione_web.nativeElement,
       [Sezioni.Sviluppo]: this.opzione_sviluppo.nativeElement,
       [Sezioni.Certificazioni]: this.opzione_certificazioni.nativeElement,
-    }
+    };
   }
 }
 
 export enum Sezioni {
   Web,
   Certificazioni,
-  Sviluppo
+  Sviluppo,
 }
