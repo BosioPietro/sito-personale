a25175f30c958f580197738e72d72d992dee5d23
Bosio Pietro <113420960+BosioPietro@users.noreply.github.com>
2025-11-02 13:37:55 +0100

    Add local DM Sans font and update styles

--- src/app/sezione-principale/sfondo/sfondo.component.ts
+++ src/app/sezione-principale/sfondo/sfondo.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'SfondoPrincipale',
-    imports: [],
-    templateUrl: './sfondo.component.html',
-    styleUrl: './sfondo.component.scss'
+  selector: 'SfondoPrincipale',
+  templateUrl: './sfondo.component.html',
+  styleUrl: './sfondo.component.scss',
 })
-export class SfondoComponent {
-
-}
+export class SfondoComponent {}
