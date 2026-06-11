5068c6d3933b6bc43fc00ef08afe868da8f7f775
Bosio Pietro <113420960+BosioPietro@users.noreply.github.com>
2026-06-11 16:42:24 +0200

    fix defaults

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,15 @@
 import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
-  provideZonelessChangeDetection,
 } from '@angular/core';
 import {
   provideClientHydration,
   withEventReplay,
-  withIncrementalHydration,
 } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
-    provideClientHydration(withEventReplay(), withIncrementalHydration()),
+    provideClientHydration(withEventReplay()),
   ],
 };
